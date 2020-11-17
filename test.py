79b7183d963d8bbe70db298c4dcc203b3c0712c3
dreamerlang <980325285@qq.com>
2020-11-18 00:38:20 +0800

    finish get_observation but not tested

--- test.py
+++ test.py
@@ -1,5 +1,17 @@
 import os
 if __name__ == '__main__':
-    dir='./result/ddpg/test'
-    if not os.path.exists(dir):
-        os.makedirs(dir)
+    list1=[4,4]
+    list2 = [5, 4]
+    list3 = [1, 3]
+    list4 = [2, 5]
+    list5 = [2, 1]
+    lists=[]
+    lists.append(list1)
+    lists.append(list2)
+    lists.append(list3)
+    lists.append(list4)
+    lists.append(list5)
+    print(lists)
+    lists.sort()
+    print(lists)
+
